20315991bf09df73d5ebf62dcadef1d7d4f1d65f
Carol217 <cpan3@stuy.edu>
2017-10-02 18:45:44 -0400

    app and templates added

---
+++ app.py
@@ -0,0 +1,33 @@
+#
+#
+#
+#
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+#assign following fxn to run when
+#root route requested
+@app.route("/")
+def hello_world():
+    return render_template('input.html')
+
+@app.route("/auth")
+def authenticate():
+    '''
+    print "this is app: ", app
+    print "================================"
+    print "what is request: ", request
+    print "================================"
+    print "this is header: ", request.headers
+    print "================================"
+    print "this is method: ", request.method
+    print "================================"
+    print "this is args: ", request.args
+    print "================================"
+    print "this is form: ", request.form
+    '''
+    return render_template('output.html', name = request.args, method = request.method)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
